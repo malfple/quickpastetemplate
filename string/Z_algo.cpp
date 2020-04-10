2fc9c93efad61e4e62113a9c254b8ed1aaaf19c0
malfple <noogai886@yahoo.com>
2020-04-10 20:57:00 +0700

    Zalgo: minor fix

--- string/Z_algo.cpp
+++ string/Z_algo.cpp
@@ -2,16 +2,16 @@
 
 l = r = 0;
 Z[0] = n;
-for(int i = 1; i < n; i++) {
+for(int i = 1; i < s.size(); i++) {
     if(i > r) { // over bound -> manual widen
         l = r = i;
-        while(r < n && s[r-l] == s[r]) r++;
+        while(r < s.size() && s[r-l] == s[r]) r++;
         Z[i] = r-l; r--;
     }else{
         if(Z[i-l] < r-i+1) Z[i] = Z[i-l];
         else{ // lcp exceeds bound -> manual widen
             l = i;
-            while(r < n && s[r-l] == s[r]) r++;
+            while(r < s.size() && s[r-l] == s[r]) r++;
             Z[i] = r-l; r--;
         }
     }
