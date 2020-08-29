7b37aca9571b66c1a4448d54a12bf67a0fc2bade
malfple <noogai886@yahoo.com>
2020-08-29 13:54:54 +0700

    update treap

--- datastructures/treap.cpp
+++ datastructures/treap.cpp
@@ -7,12 +7,14 @@ Other query/update/propagation techniques can be used as well.
  */
 
 // the BBST feature will no longer function if using as dynamic array
+// supports reversing (an example of lazy propagation)
 struct node{
     int key, sz; // key -> bbst, sz -> dynamic array
     int v;
     int pri; // priority
+    bool flip;
     node* l; node* r;
-    node(int key, int v) : key(key), sz(1), v(v), pri(rand()), l(NULL), r(NULL){}
+    node(int key, int v) : key(key), sz(1), v(v), pri(rand()), flip(0), l(NULL), r(NULL){}
 }*head;
 
 int getsize(node* now){
@@ -24,10 +26,24 @@ void update(node* now){
         now->sz = getsize(now->l) + getsize(now->r) + 1;
     }
 }
+void apply(node* now){
+    if(now){
+        now->flip ^= 1;
+        swap(now->l, now->r);
+    }
+}
+void prop(node* now){
+    if(now && now->flip){
+        now->flip = 0;
+        apply(now->l);
+        apply(now->r);
+    }
+}
 
 // current function: dynamic array. See comments for bbst
 void split(node* now, int k, node*& a, node*& b){ // <=k, >k
     if(!now){a = b = NULL;return;}
+    prop(now);
     if(getsize(now->l) + 1 <= k){ // now->key <= k
         a = now;
         split(now->r, k-getsize(now->l)-1, a->r, b); // (now->r, k, a->r, b)
@@ -40,6 +56,7 @@ void split(node* now, int k, node*& a, node*& b){ // <=k, >k
 
 node* join(node* a, node* b){
     if(!a)return b; if(!b)return a;
+    prop(a); prop(b);
     if(a->pri < b->pri){
         a->r = join(a->r, b);
         update(a);
@@ -56,6 +73,7 @@ node* join(node* a, node* b){
 
 // insert after node k. so 0<=k<=size
 void insert(node*& now, int k, node* ins){ // (now, ins) no need k
+    prop(now);
     if(!now)now = ins;
     else if(ins->pri > now->pri){
         split(now, k, ins->l, ins->r), now = ins; // by index => by key
@@ -67,20 +85,29 @@ void insert(node*& now, int k, node* ins){ // (now, ins) no need k
     update(now);
 }
 // if erase key, have to check if key exists; if(!now)
-void erase(node*& now, int k){ // 1<=k<=size
+node* erase(node*& now, int k){ // 1<=k<=size
+    prop(now);
     int sl = getsize(now->l); // no need sl
+    node* erased = NULL;
     if(sl+1 == k){ // now->key == k
-        node* t = join(now->l, now->r);
-        delete now; now = t;
+        erased = now;
+        now = join(now->l, now->r);
     }else{
         // (k < now->key ? now->l : now->r, k)
-        erase(k < sl+1 ? now->l : now->r, k < sl+1 ? k : k-sl-1);
+        erased = erase(k < sl+1 ? now->l : now->r, k < sl+1 ? k : k-sl-1);
     }
     update(now);
+    return erased;
 }
 
+/*
+reversing (l, r)
+split so you get the treap in range (l, r)
+apply(the head node of (l,r))
+join everything back
+*/
 
-// class version
+// class version (deprecated)
 
 template<typename __key, typename __val>
 class Treap{
