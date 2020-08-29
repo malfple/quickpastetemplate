b8ba12a10e2c651b045782930914aa88df4a5742
malfple <noogai886@yahoo.com>
2020-08-29 12:30:11 +0700

    Update Treap

--- datastructures/treap.cpp
+++ datastructures/treap.cpp
@@ -6,51 +6,71 @@ for storing size of subtree (cartesian tree),
 Other query/update/propagation techniques can be used as well.
  */
 
+// the BBST feature will no longer function if using as dynamic array
 struct node{
-    int key; int v;
-    int pri;
+    int key, sz; // key -> bbst, sz -> dynamic array
+    int v;
+    int pri; // priority
     node* l; node* r;
-    node(int key) : key(key), v(0), pri(rand()), l(NULL), r(NULL){}
+    node(int key, int v) : key(key), sz(1), v(v), pri(rand()), l(NULL), r(NULL){}
 }*head;
 
+int getsize(node* now){
+    if(now)return now->sz;
+    return 0;
+}
+void update(node* now){
+    if(now){
+        now->sz = getsize(now->l) + getsize(now->r) + 1;
+    }
+}
+
+// current function: dynamic array. See comments for bbst
 void split(node* now, int k, node*& a, node*& b){ // <=k, >k
     if(!now){a = b = NULL;return;}
-    if(now->key <= k){
+    if(getsize(now->l) + 1 <= k){ // now->key <= k
         a = now;
-        split(now->r, k, a->r, b);
+        split(now->r, k-getsize(now->l)-1, a->r, b); // (now->r, k, a->r, b)
     }else{
         b = now;
         split(now->l, k, a, b->l);
     }
-    //pull(now);
+    update(now);
 }
 
 node* join(node* a, node* b){
     if(!a)return b; if(!b)return a;
     if(a->pri < b->pri){
         a->r = join(a->r, b);
-        //pull(a);
+        update(a);
         return a;
     }
     b->l = join(a, b->l);
-    //pull(b);
+    update(b);
     return b;
 }
 
-node* insert(int key){ // returns added node
-    node* tmp,* add = new node(key);
-    split(head, key, head, tmp);
-    head = join(join(head, add), tmp);
-    return add;
-}
+/*
+quick insert:
+    if now null
+        now = new node
+    if current node has higher priority than new node
+        descent the tree like usual
+    else
+        split -> assign as children to new node
+    update(now)
 
-void erase(int key){
-    node* target,* tmp;
-    split(head, key, head, tmp);
-    split(head, key-1, head, target);
-    head = join(head, tmp);
-    if(target)delete target;
-}
+quick erase:
+    if now null
+        well, do nothing
+    if found
+        join the deleted node's children
+    else
+        descent the tree like usual
+    update(now)
+
+the slow variant = just split everything lmao
+*/
 
 
 // class version
